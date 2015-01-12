6ba33541d6c291b679c24463fa809e39e21a0f03
power10dan <lintzu@onid.oregonstate.edu>
2015-01-12 00:58:19 -0800

    GridView for Tea Customization Working, but we need good pictures

--- app/src/main/java/Cards/CustomizationCard.java
+++ app/src/main/java/Cards/CustomizationCard.java
@@ -3,40 +3,55 @@ package Cards;
 import android.content.Context;
 
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import android.view.View;
 import android.view.ViewGroup;
 
-
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardThumbnail;
+
+import com.squareup.picasso.Picasso;
 import com.t_danbubbletea.bubbleteaapp.R;
 
 public class CustomizationCard extends Card{
 
-    public CustomizationCard(Context context, int innerLayout){
+    String customizationType;
+    int imageDrawable;
+    public CustomizationCard(Context context, int innerLayout, String customType, int drawableImg){
         super(context, innerLayout);
+        customizationType = customType;
+        imageDrawable = drawableImg;
         init();
     }
 
     private void init(){
-        //Add thumbnail
-        CardThumbnail cardThumbnail = new CardThumbnail(mContext);
-        cardThumbnail.setDrawableResource(R.drawable.carddemo_ic_gmaps);
-        addCardThumbnail(cardThumbnail);
-
-
-
+        IconThumbnail iconThumb = new IconThumbnail(getContext());
+        iconThumb.setExternalUsage(true);
+        addCardThumbnail(iconThumb);
     }
 
     @Override
     public void setupInnerViewElements(ViewGroup parent, View view){
-        TextView customizeOptions = (TextView) view.findViewById(R.id.);
+        TextView customizeOptions = (TextView) view.findViewById(R.id.custom_card_title);
+        customizeOptions.setText(customizationType);
+    }
 
-        customizeOptions.setText("Milk type");
+    class IconThumbnail extends CardThumbnail {
 
+        public IconThumbnail(Context context) {
+            super(context);
+        }
 
+        @Override
+        public void setupInnerViewElements(ViewGroup parent, View viewImage) {
 
+            Picasso.with(getContext())
+                    .load(imageDrawable)
+                    .into((ImageView) viewImage);
 
+            viewImage.getLayoutParams().width = 190;
+            viewImage.getLayoutParams().height = 190;
+        }
     }
 }
--- app/src/main/java/FragmentPages/TeaDetailFragment.java
+++ app/src/main/java/FragmentPages/TeaDetailFragment.java
@@ -1,5 +1,7 @@
 package FragmentPages;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -12,30 +14,75 @@ import android.view.ViewGroup;
 import com.squareup.picasso.Picasso;
 import com.t_danbubbletea.bubbleteaapp.R;
 
+import Cards.CustomizationCard;
 import Cards.TeaDescriptionCard;
+
 import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardGridArrayAdapter;
 import it.gmariotti.cardslib.library.view.CardViewNative;
+import it.gmariotti.cardslib.library.view.CardGridView;
 
 public class TeaDetailFragment extends Fragment {
 
+    int [] drawable = {R.drawable.flavorthumb, R.drawable.milkthumb, R.drawable.tempthumb,
+                        R.drawable.weightthumb};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
 
         View rootView = inflater.inflate(R.layout.tea_detail_layout, container, false);
+        setUpTeaPage(rootView);
+        setUpCardGrid(rootView);
 
-        ImageView teaImage = (ImageView) rootView.findViewById(R.id.tea_image);
+        return rootView;
 
+    }
+
+    private void setUpTeaPage(View view){
+        // set up tea profile image
+        ImageView teaImage = (ImageView) view.findViewById(R.id.tea_image);
         Picasso.with(getActivity())
                 .load("http://chuangmi.my-place.us/Tea_Pictures/assam.jpg")
                 .into(teaImage);
 
+        // set up description card
         TeaDescriptionCard descriptCard = new TeaDescriptionCard(getActivity());
         //Set card in the cardView
-        CardViewNative cardView = (CardViewNative) rootView.findViewById(R.id.description_card);
+        CardViewNative cardView = (CardViewNative) view.findViewById(R.id.description_card);
         cardView.setCard(descriptCard);
 
-        return rootView;
+    }
+
+    private void setUpCardGrid(View view){
+
+        ArrayList<Card> arrayGridCards = new ArrayList<>();
+        // add customization card
+        CustomizationCard customCardFlavor = new CustomizationCard(getActivity()
+                , R.layout.customization_text_layout
+                , "Flavor", drawable[0]);
+        arrayGridCards.add(customCardFlavor);
+
+        CustomizationCard customCardMilk = new CustomizationCard(getActivity()
+                , R.layout.customization_text_layout
+                , "Milk", drawable[1]);
+        arrayGridCards.add(customCardMilk);
+
+        CustomizationCard customCardTemp = new CustomizationCard(getActivity()
+                , R.layout.customization_text_layout
+                , "Temperature", drawable[2]);
+        arrayGridCards.add(customCardTemp);
+
+        CustomizationCard customCardWeight = new CustomizationCard(getActivity()
+                , R.layout.customization_text_layout
+                , "Weight", drawable[3]);
+        arrayGridCards.add(customCardWeight);
+
+        // set up card array adapter
+        CardGridArrayAdapter cardGridArrayAdapter = new CardGridArrayAdapter(getActivity()
+                                                                             ,arrayGridCards);
+
+        CardGridView customTeaGridView = (CardGridView) view.findViewById(R.id.tea_customize_grid);
+        customTeaGridView.setAdapter(cardGridArrayAdapter);
 
     }
 }
